e3d49d45cb251a8a085a8495ded61f31b4faf0f6
Paul Wenzel <wenzel.pgb@googlemail.com>
2016-08-17 23:49:44 -0700

    Untested implementation of the DocumentLoader. (Forgot to added to the priviouse commit.)

---
+++ client/app/shared/databases/board/board_document_loader.ts
@@ -0,0 +1,29 @@
+import {PouchDbDocument} from "../../utils/pouch_db/pouch_db_document";
+import {PouchDbDocumentLoaderInterface} from "../../utils/pouch_db/pouch_db_document_loader_interface";
+import {BoardDocument} from "./board_document";
+
+/**
+ * COUTION: This abstract class is supposed to be an interface! It will eventually get implemented
+ * by a class that implements the abstract {@link PouchDbDatabase} class. I could not use the type
+ * interface for this file, since I want to use it as a token for dependency injection later on.
+ * Since interfaces are not available under JavaScript, interfaces can not be used as tokens for dependency
+ * injection. Any implementation done in this file, will NOT be available for the class that implements it,
+ * since this class does not extend but implement this file! This being said means, that this file should
+ * ONLY define abstract classes as if it would be an interface.
+ *
+ *
+ * This interface is the BoardDocumentLoader and defines the functions which will be provided for the controllers to get Board-Document-Objects.
+ *
+ * A Document-Loader enables a controller to get Document-Objects from a database. A Document-Loader only implements
+ * functions for getting, deleting or creating new documents. (e.g. a function for retrieving all documents with the property dueDate=today)
+ *
+ * This interface calls gets implemented by the {@link BoardDatabase}. The controllers should only access the {@link BoardDatabase} by using the functions
+ * of this interface. Doing so prevents a controller form setting the URL of a database or uploading documents to the database. Uploading documents
+ * to the database is not necessary since the Document-Objects retrieved from a database do automatically sync in both directions with the database.
+ */
+export abstract class BoardDocumentLoader extends PouchDbDocumentLoaderInterface<BoardDocument> {
+
+/////////////////////////////////////////////Methods///////////////////////////////////////////////
+
+
+}
