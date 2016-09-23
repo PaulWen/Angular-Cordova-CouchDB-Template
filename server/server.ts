67195ba674eab83f47a42992a90de43617007cce
Paul Wenzel <wenzel.pgb@googlemail.com>
2016-09-22 21:54:06 -0700

    Added npm script for enabling CORS at CouchDB

--- server/server.ts
+++ server/server.ts
@@ -77,7 +77,7 @@ let superlogin = new SuperLogin(config);
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    next()
+    next();
 });
 
 // output request details
